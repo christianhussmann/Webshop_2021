aa336b777eaaa75310022b7bc1a8680b943fd0e7
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-11-09 10:42:45 +0100

    Added infrastructure

---
+++ EASV.Webshop2021.DB/WebShopDBContext.cs
@@ -0,0 +1,15 @@
+﻿using EASV.Webshop2021.DB.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EASV.Webshop2021.DB
+{
+    public class WebShopDbContext : DbContext
+    {
+        public WebShopDbContext(DbContextOptions<WebShopDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
