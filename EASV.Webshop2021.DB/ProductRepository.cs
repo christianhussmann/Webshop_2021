fc9792415d289667a02ac623f6dd6043c89ec8f8
Mathias Kristensen <mathiaskristensen52@gmail.com>
2021-11-09 10:50:15 +0100

    More plumbing

---
+++ EASV.Webshop2021.DB/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using EASV.Webshop2021.Core.Models;
+using EASV.Webshop2021.Domain.IRepositories;
+
+namespace EASV.Webshop2021.DB
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly WebShopDbContext _ctx;
+
+        public ProductRepository(WebShopDbContext context)
+        {
+            _ctx = context;
+        }
+        
+        public List<Product> ReadAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product CreateProduct(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
